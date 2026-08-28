0ccd3f0061e4fe98d80e741964db23ccdb2504d5
Muhammad Aqib <aqibk1051@gmail.com>
2026-08-28 15:04:53 +0500

    Backend Migrated to Supabase

--- backend/src/routes/reviewRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { addReview } = require('../controllers/reviewController');
-const { protect, authorize } = require('../middleware/authMiddleware');
-
-const router = express.Router();
-
-router.route('/:workerId').post(protect, authorize('customer'), addReview);
-
-module.exports = router;
